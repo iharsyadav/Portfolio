de9f30f07f08534456cf9fc82453e964560d7b3c
harsh yadav <iharsyadav@gamil.com>
2025-12-14 13:16:09 +0530

    upgate

--- my-portfolio/src/Homepage.jsx
+++ my-portfolio/src/Homepage.jsx
@@ -1,35 +1,32 @@
 import React from "react";
-import Home from "./component/home/home";
-import About from "./component/about/about";
+import Home from "./component/home/Home";
+import About from "./component/about/About";
 import Contact from "./component/contact/Contact";
-import Mywork from "./component/mywork/mywork";
-import Blogs from "./component/Blogs/Blogs";
-import Footer from "./component/Footer/Footer";
 import Portfolio from "./component/Portfolio/Portfolio";
-import Resume from "./component/Resume/Resume";
 import Slidebar from "./component/slidebar/slidebar";
-import Otherskill from "./component/otherskill/otherskill";
+import Otherskill from "./component/otherskill/Otherskill";
 const Homepage = () => {
   return (
-    <>  <Slidebar />
+    <>
+    
+      <Slidebar />
       <div className="main">
         <div>
           <Home />
         </div>
 
-        <div style={{marginTop:"12rem"}} className="">
-           <About/>
+        <div style={{ marginTop: "12rem" }} className="">
+          <About />
         </div>
         <div>
-          <Otherskill/>
-        </div>
-         <div>
-          <Portfolio/>
+          <Otherskill />
         </div>
         <div>
-          <Contact/>
+          <Portfolio />
         </div>
-       
+        <div>
+          <Contact />
+        </div>
       </div>
     </>
   );
