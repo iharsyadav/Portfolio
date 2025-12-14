739474378d367eaf1465b0d872e312bbf482e731
harsh yadav <iharsyadav@gamil.com>
2025-12-14 12:44:32 +0530

    Fix case-sensitive import path

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './component/home/home';
 import Homepage from './homepage';
 import './App.css'
 function App() {
